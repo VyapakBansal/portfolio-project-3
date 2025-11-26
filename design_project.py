071e409b8899bb0e643e7ffa2be7efb0ba7cb140
MattG67 <matthew.p.guillaume@gmail.com>
2025-11-26 11:14:27 -0700

    Added files

---
+++ design_project.py
@@ -0,0 +1,8 @@
+# design_project.py
+# ENDG 233 F24
+# STUDENT NAME(S)
+# GROUP NAME
+# A terminal-based data analysis and visualization program in Python.
+# You must follow the specifications provided in the project description.
+# Remember to include docstrings and comments throughout your code.
+
---
+++ user_csv.py
@@ -0,0 +1,26 @@
+# user_csv.py
+# ENDG 233 F24
+# STUDENT NAME(S)
+# GROUP NAME
+# A terminal-based data analysis and visualization program in Python.
+# You must follow the specifications provided in the project description.
+# Remember to include docstrings and comments throughout your code.
+
+def read_csv(filename, include_headers = True):
+    """Reads the contents of a CSV file and returns a 2D list
+    
+    Parameters:
+    filename(str): The name of the CSV to be read
+    include_headers(bool): whether or not to return the headers as part of the output, defaults to True
+    
+    Returns:
+    data_list(lst): A 2D list of the CSV file, with or without headers"""
+
+
+def write_csv(filename, data, overwrite):
+    """Takes a 2D list and writes it in to a CSV file
+
+    Parameters:
+    filename(str): name of the file you want to write the CSV into
+    data(list): the 2D list to write into the CSV
+    overwrite(bool): whether to overwrite the existing data or appending the new data"""
