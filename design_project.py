9535f922e502193c6d6e39a4ce3d0ca714c7df7b
VyapakBansal <167020115+VyapakBansal@users.noreply.github.com>
2025-11-26 18:11:22 -0700

    max_endangered

--- design_project.py
+++ design_project.py
@@ -1,43 +1,46 @@
 # design_project.py
 # ENDG 233 F24
 # Matthew Guillaume, Vyapak Bansal
-# GROUP NAME
+# Group 4
 # A terminal-based data analysis and visualization program in Python.
 # You must follow the specifications provided in the project description.
 # Remember to include docstrings and comments throughout your code.
+import user_csv
 
-def get_ave_population(region, subregion = False, year = '2020', country_data, population_data):
-    """Finds the the average population of a region, with an optional subregion
-    Parameters:
-    region(str): A valid region 
-    subregion(str): An optional specification for a valid sub-region 
-    year(str): The year that to take the population data from, defaults to 2020
-    country_data(array): A numpy array containing all the country data
-    population_data(array): A numpy array containing information on endagered species 
-    Returns:
-    ave_population(flt): the average population in a region"""
 
-def get_max_endagered_species(region, subregion = False):
+def get_max_endagered_species(region = False, subregion = False):
     """Finds the country with the highest endagered species of a region, with an optional subregion
     Parameters:
-    region(str): A valid region 
-    subregion(str): An optional specification for a valid sub-region 
+        region(str): A valid region 
+        subregion(str): An optional specification for a valid sub-region 
     Returns:
-    max_endagered_species(lst): a list containg the country and it's number of endagered species"""
+        max_endagered_species(lst): a list containg the countries and it's number of endagered species""" 
+    # @Matt, can you fix the docstring here to reflect this? I am not able to get the exact things to say.
+
+    total_species = 0
+    max_species = 0
+    countries = []
+    data = user_csv.read_csv("./data_files/Threatened_Species.csv", include_headers=False)
+    for country_profile in data:
+        total_species = sum(country_profile[1:])
+        if total_species >= max_species:
+            max_species = total_species
+            countries.append(country_profile[0])
+    return f"The maximum endangered species: {max_species}.\nThe countries are {", ".join([country for country in countries])}"
+    
 
 def get_population_density(country, year = '2020'):
-        """Finds the population density in a given country and year 
+    """Finds the population density in a given country and year 
     Parameters:
-    country(str): A valid country 
-    year(str): The year that to take the population data from, defaults to 2020
+        country(str): A valid country 
+        year(str): The year that to take the population data from, defaults to 2020
     Returns:
-    population_density(flt): the ratio of population per square kilometer"""
+        population_density(flt): the ratio of population per square kilometer"""
 
 def plot_pop_and_endSpec(country):
-     """Plots a feature with 2 subplots, one that shows a countrys population over time and one 
+    """Plots a feature with 2 subplots, one that shows a countrys population over time and one 
         breaks down the number of endagered species in that country
     Parameters:
-    country(str): A valid country
-        
+        country(str): A valid country
     Returns: 
-    2 graphs on the same figure showing population and endagered species"""
+        2 graphs on the same figure showing population and endagered species"""
--- user_csv.py
+++ user_csv.py
@@ -14,31 +14,30 @@ def read_csv(file_name, include_headers = True):
     Parameters:
     filename(str): The name of the CSV to be read
     include_headers(bool): whether or not to return the headers as part of the output, defaults to True
-    
     Returns:
     data_list(lst): A 2D list of the CSV file, with or without headers"""
-    with open(file_name, "r") as country_data:
-        for index, line in enumerate(country_data):
-            if index == 0:
-                continue
+    rows = []
+    with open(file_name, "r") as file:
+        for index, line in enumerate(file):
+            if not include_headers:
+                if index == 0:
+                    continue
             row = line.strip().split(",")
             for index, element in enumerate(row):
-                if index == 3:
-                    if element == '':
-                        continue
-                    row[3] = float(element)
-            array = np.array(row , dtype=object)
-            print(array)
+                if element.isnumeric():
+                    row[index] = float(element)                  
+            rows.append(row)
+        array = np.array(rows , dtype=object)
+    return array
 
-
-read_csv("./data_files/Country_Data.csv")
 def write_csv(filename, data, overwrite):
-    """Takes a 2D list and writes it in to a CSV file
-
+    """
+    Takes a 2D list and writes it in to a CSV file
     Parameters:
-    filename(str): name of the file you want to write the CSV into
-    data(list): the 2D list to write into the CSV
-    overwrite(bool): whether to overwrite the existing data or appending the new data"""
+        filename(str): name of the file you want to write the CSV into
+        data(list): the 2D list to write into the CSV
+        overwrite(bool): whether to overwrite the existing data or appending the new data
+    """
 
     
     # Check to see if we are writing or appending
